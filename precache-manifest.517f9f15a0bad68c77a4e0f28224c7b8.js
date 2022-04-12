79e63aba644e08032f1424567bbdbbdff3c95843
shadowwalker1025 <nagendra.tere@gmail.com>
2022-04-12 10:55:14 +0530

    Updates

---
+++ precache-manifest.517f9f15a0bad68c77a4e0f28224c7b8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c6041e078e6481a30e6",
+    "url": "/ReactJs_Project_1/static/css/main.837b06ee.chunk.css"
+  },
+  {
+    "revision": "2c6041e078e6481a30e6",
+    "url": "/ReactJs_Project_1/static/js/main.2c6041e0.chunk.js"
+  },
+  {
+    "revision": "df73d79c6fed95831fac",
+    "url": "/ReactJs_Project_1/static/js/1.df73d79c.chunk.js"
+  },
+  {
+    "revision": "f3e51023560a26039c4a",
+    "url": "/ReactJs_Project_1/static/js/runtime~main.f3e51023.js"
+  },
+  {
+    "revision": "75dcf3d94424ae99fbb7667854efd244",
+    "url": "/ReactJs_Project_1/index.html"
+  }
+];
